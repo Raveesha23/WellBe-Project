f0d76c64fbd10c6e9c4276a2544b8f5ba041b705
Amrah444 <ammuslamath@gmail.com>
2024-11-07 20:36:35 +0530

    config changed

--- app/core/config.php
+++ app/core/config.php
@@ -9,7 +9,7 @@ if($_SERVER['SERVER_NAME'] == 'localhost')
 	define('DBPASS', '');
 	define('DBDRIVER', '');
 	
-	define('ROOT', 'http://localhost/WellBe-Project/public');
+	define('ROOT', 'http://localhost/mvc/public');
 
 }else
 {
